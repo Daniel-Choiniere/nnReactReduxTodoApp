0ee45658c64ae873b61a5953a5c283341fc29fe7
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-03 02:02:57 +0000

    succesfully delting todo items when clicked

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ class App extends Component {
     ]
   }
   deleteTodo = (id) => {
-    console.log(id);
+    // The filter() method creates an array filled with all array elements that pass a test (provided as a function). That new array is saved in the todos variable 
+    const todos = this.state.todos.filter(todo => {
+      // return true (for all todos with ids that dont match the current id) and add all those elements to the new todos array
+      return todo.id !== id;
+    });
+    this.setState({
+      // update the state to the new todos array
+      todos: todos
+    });
   }
   render() {
     return (
