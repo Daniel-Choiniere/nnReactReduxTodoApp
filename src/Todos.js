734aa20a8a1bdef928753f15882a383d199603a6
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-03 00:57:22 +0000

    created a component to display the list of todos, and also working on one to delete a todo item

--- src/Todos.js
+++ src/Todos.js
@@ -1,26 +1,27 @@
 import React from 'react'
 
-// grabbing the todos off the prop object
-const Todos = ({todos}) => {
+// grabbing the todos, and deleteTodo off the prop object using destructuring
+const Todos = ({todos, deleteTodo}) => {
     
     // tereneary operator. If there are todos than the function to the right of the question mark is fired. if there are no todos than the function to the right of the colon is fired
     const todoList = todos.length ? (
+        // The map() method calls the provided function once for each element in an array, in order.
         todos.map(todo => {
             return (
                 // React expects unique key for every surrounding element that we return with the map function
                 <div className="collection-item" key={todo.id}>
-                    <span>{todo.content}</span>
+                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
                 </div>
-            )
+            );
         })
         ) : (
         <p className="center">You have no todos left, yay!</p>
-        )
+        );
     return(
         <div className="todos collection">
             {todoList}
         </div>
-    )
-}
+    );
+};
 
 export default Todos;
